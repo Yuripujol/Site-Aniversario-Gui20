9c300d9a610ca9064c2e0ca0b418f27c31480dc4
yuripujolYuripujol <yuripujolcorrea@gmail.com>
2024-09-26 21:36:31 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,47 @@
+  // AUDIO PARA O BOTÃO lars//
+
+  const audiolars = new Audio();
+
+  audiolars.src = "./assets/Lars(1).MP3";
+
+  // AUDIO PARA O BOTÃO coringa //
+
+  const audiocoringa = new Audio();
+
+  audiocoringa.src = "./assets/coringa.MP3";
+
+  // AUDIO PARA O BOTÃO piratas//
+
+  const audiopiratas = new Audio();
+
+  audiopiratas.src = "./assets/piratas(1).MP3";
+
+    // AUDIO PARA O BOTÃO picapau//
+
+    const audiopicapau = new Audio();
+
+    audiopicapau.src = "./assets/Risada pica-pau.MP3";
+
+      // AUDIO PARA O BOTÃO pum//
+
+  const audiopum = new Audio();
+
+  audiopum.src = "./assets/minha vida é que nem um pum.MP3";
+
+    // AUDIO PARA O BOTÃO cowboy //
+
+    const audiocowboy = new Audio();
+
+    audiocowboy.src = "./assets/Tráso bizerro !.MP3";
+ 
+     // AUDIO PARA O BOTÃO introdução //
+
+     const audiofealves = new Audio();
+
+     audiofealves.src = "./assets/Entre no flow comigo.MP3";
+   
+       // AUDIO PARA O BOTÃO gui //
+
+       const audiogui = new Audio();
+
+       audiogui.src = "./assets/Áudio do gui.mp3";
